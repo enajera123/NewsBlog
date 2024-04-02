ccf65135a91f60d4d48e3fc25cec3908f249a712
Esteban Nájera <enajera@MacBook-Air-de-Esteban.local>
2024-04-01 20:04:20 -0600

    [ADD] Componentes added

---
+++ src/app/components/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
+      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
+      <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table.</p>
+    </div>
+  )
+}
+
+export default Dashboard
